b5036ecff87196b574736f7e294503536bfef46d
Firew0lf <Firew0lf@users.noreply.github.com>
2015-10-02 08:38:35 +0200

    Added "sdmc:/3ds/ctruLua/libs/" to the libraries path
    You can now require("sprite") !

--- libs/lua-5.3.1/src/luaconf.h
+++ libs/lua-5.3.1/src/luaconf.h
@@ -188,12 +188,14 @@
 
 #else			/* }{ */
 
-#define LUA_ROOT	"/usr/local/"
-#define LUA_LDIR	LUA_ROOT "share/lua/" LUA_VDIR "/"
-#define LUA_CDIR	LUA_ROOT "lib/lua/" LUA_VDIR "/"
+#define LUA_ROOT	"sdmc:/"
+#define LUA_LDIR	LUA_ROOT "3ds/ctruLua/libs/"
+#define LUA_CDIR	LUA_ROOT "3ds/ctruLua/libs/"
 #define LUA_PATH_DEFAULT  \
-		LUA_LDIR"?.lua;"  LUA_LDIR"?/init.lua;" \
-		LUA_CDIR"?.lua;"  LUA_CDIR"?/init.lua;" \
+		LUA_LDIR"?.lua;"          LUA_LDIR"?/init.lua;" \
+		LUA_LDIR LUA_VDIR"?.lua;" LUA_LDIR LUA_VDIR"?/init.lua;" \
+/*		LUA_CDIR"?.lua;"          LUA_CDIR"?/init.lua;" \
+		LUA_CDIR LUA_VDIR"?.lua;" LUA_CDIR LUA_VDIR"?/init.lua;" \ */
 		"./?.lua;" "./?/init.lua"
 #define LUA_CPATH_DEFAULT \
 		LUA_CDIR"?.so;" LUA_CDIR"loadall.so;" "./?.so"
