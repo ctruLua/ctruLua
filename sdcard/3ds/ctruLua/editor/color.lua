716c42b849d017f69726eb83709efed0774066bd
Reuh <email.thomas99@gmail.com>
2015-12-24 13:15:00 +0100

    Fixed keyboard and editor colors

--- sdcard/3ds/ctruLua/editor/color.lua
+++ sdcard/3ds/ctruLua/editor/color.lua
@@ -1,16 +1,18 @@
+local hex = require("ctr.gfx.color").hex
+
 -- Colors based on the Monokai theme
 return {
 	-- General
-	["background"] = 0x272822FF,
-	["cursor"] = 0xF8F8F0FF,
-	["default"] = 0xF8F8F2FF,
+	["background"] = hex(0x272822FF),
+	["cursor"] = hex(0xF8F8F0FF),
+	["default"] = hex(0xF8F8F2FF),
 
 	-- Syntax
-	["comment"] = 0x75715EFF,
-	["string"] = 0xE6DB74FF,
-	["constant.numeric"] = 0xAE81FFFF,
-	["constant.language"] = 0xAE81FFFF,
-	["keyword.control"] = 0xF92672FF,
-	["keyword.operator"] = 0xF92672FF,
-	["support.function"] = 0x66D9EFFF
+	["comment"] = hex(0x75715EFF),
+	["string"] = hex(0xE6DB74FF),
+	["constant.numeric"] = hex(0xAE81FFFF),
+	["constant.language"] = hex(0xAE81FFFF),
+	["keyword.control"] = hex(0xF92672FF),
+	["keyword.operator"] = hex(0xF92672FF),
+	["support.function"] = hex(0x66D9EFFF)
 }
