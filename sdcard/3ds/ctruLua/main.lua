c053997f96ec0a4834afb8efaf8a9d3624829ba6
Reuh <email.thomas99@gmail.com>
2015-12-29 17:20:29 +0100

    Reset defaults before starting a file (main.lua)

--- sdcard/3ds/ctruLua/main.lua
+++ sdcard/3ds/ctruLua/main.lua
@@ -1,3 +1,4 @@
+local gfx = require("ctr.gfx")
 local fs = require("ctr.fs")
 
 -- Set up path
@@ -5,12 +6,15 @@ local ldir = fs.getDirectory().."libs/"
 package.path = package.path..";".. ldir.."?.lua;".. ldir.."?/init.lua"
 
 repeat
+	gfx.set3D(false)
+	gfx.color.setDefault(0xFFFFFFFF)
+	gfx.color.setBackground(0xFF000000)
+	gfx.font.setDefault()
 	local file = require("openfile")("Choose a Lua file to execute", nil, ".lua", "exist")
 	if file then
 		fs.setDirectory(file:match("^(.-)[^/]*$"))
 		local success, err = pcall(dofile, file)
 		if not success then
-			local gfx = require("ctr.gfx")
 			local hid = require("ctr.hid")
 			gfx.set3D(false)
 			gfx.color.setDefault(0xFFFFFFFF)
